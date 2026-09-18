25272c1a3f198547f7f140e7ae8530f1188f218b
Kumar Aman <kumaraman4213r@gmail.com>
2026-09-18 19:18:00 +0530

    Day03

---
+++ index.js
@@ -0,0 +1,52 @@
+//Operator
+
+//Aritmetic
+console.log(2+5);
+console.log(2-5);
+console.log(2*5)
+console.log(6/2)
+console.log(5%2) // remainder
+
+console.log(5**2)
+
+//Assignment Operators
+
+let x = 20
+let y = 10
+//x = x+y same as  x+=y
+// x = x-y
+// x = x*y
+x*=y
+console.log(x)
+
+//Comparsion Opertor
+
+let a=10,
+    b=20
+console.log(a<b) 
+console.log(a<=b)    
+console.log(a>b) 
+console.log(a==b)
+
+//number to strig
+let c = 10
+let d = String(c)
+console.log(d)
+console.log(typeof d)
+
+console.log(Number(true))
+
+//computer science problem
+let e = 0.1
+let f = 0.2
+
+ g = e + f
+console.log(g)
+
+//1. null is loosely equal to undefined only
+ console.log(null==undefined)
+ console.log(null===undefined) /// ===means comparing from diffrent data  types
+
+
+
+
